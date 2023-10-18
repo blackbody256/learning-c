14c1d9561b6c8d46730413e4bf5da1ba85eb0c23
blackbody256 <sseluyindaeva@gmail.com>
2023-10-18 09:21:21 +0300

    Create calculaor of two values.c

---
+++ calculaor of two values.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main(){
+char operator;
+double First_Num;
+double Second_Num;
+printf("Enter the value of the equation:\n");
+printf("Note it has to be in form of \"first value operator second value.\"\n");
+printf("operators allowed are \"-+*/\"\n");
+scanf("%lf%c%lf", &First_Num, &operator, &Second_Num);
+switch(operator){
+    case '+' :
+        printf("%.3f", First_Num+Second_Num);
+        break;
+    case '-' :
+        printf("%.3f", First_Num-Second_Num);
+        break;
+    case '/' :
+        printf("%.3f", First_Num/Second_Num);
+        break;
+    case '*' :
+        printf("%.3f", First_Num*Second_Num);
+        break;
+    default:
+        printf("The \"%c\" Entered is not supported", &operator);
+
+
+}
+return 0;}
