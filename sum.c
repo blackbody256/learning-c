526752cbdec655943cb7657edccb3734720cf7a7
blackbody256 <sseluyindaeva@gmail.com>
2023-10-18 09:21:45 +0300

    Create sum.c

---
+++ sum.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void){
+int a;
+int b;
+int c;
+printf("enter the values");
+scanf("%d\n,%d\n,%d\n",&a,&b,&c);
+int sum = a+b+c
+printf("The sum is %d",sum);
+    return (0);
+}
