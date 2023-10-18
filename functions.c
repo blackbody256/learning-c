7d28db1ceb6b58895f38efe1612e7f37d1063674
blackbody256 <sseluyindaeva@gmail.com>
2023-10-18 09:21:27 +0300

    Create functions.c

---
+++ functions.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void birthday(){
+    printf("happy birthday to you\nHappy birthday to you\nHappy birthday happy bithday\nHappy birthday to you\n");
+    printf("How Old are You now\nHow old are you now\n");
+
+}
+int main(){
+    birthday();
+    birthday();
+    birthday();
+
+    return 0;
+}
