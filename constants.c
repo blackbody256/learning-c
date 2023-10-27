6e6e6b366c155503e168712bf004e480542cc1b0
blackbody256 <sseluyindaeva@gmail.com>
2023-10-27 16:55:47 +0300

    Create constants.c

---
+++ constants.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    //A constant is a fixed value that can't be altered by the program during it's execution
+    const float PI = 3.14245;
+    //PI = 7;it prints out an error coz PI is a constant and can't be changed
+    printf("%f", PI);
+
+
+
+
+return 0;}
