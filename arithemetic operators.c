5290458300cea42a154d8d57e099de5c32a17629
blackbody256 <sseluyindaeva@gmail.com>
2023-10-27 16:54:55 +0300

    Create arithemetic operators.c

---
+++ arithemetic operators.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main(){
+    /*arithemetic operators are common math operators suchas:
+    + (addition)
+    - (substraction)
+    / (division)
+    * (multiplication)
+    % (modulus)
+    ++ (increment)
+    -- (decrement)*/
+    int x = 5;
+    int y = 2;
+    printf("%d\n", x+y);
+    printf("%d\n", x-y);
+    printf("%d\n", x*y);
+    printf("%f\n", (float)(x) /y);
+    printf("%d\n", x%y);
+    printf("%d\n", x++);
+    printf("%d\n",y--);
+    return 0;
+}
