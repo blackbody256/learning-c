ec8f383212103c6405d9e148a9ce179acab59307
blackbody256 <sseluyindaeva@gmail.com>
2023-10-29 15:59:50 +0300

    Create numbers.c

---
+++ numbers.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main(){
+printf("%f\n", pow(2,3));//this powers the first number by the second number
+printf("%f\n", sqrt(36));//this finds the square root of the number
+return 0;
+}
