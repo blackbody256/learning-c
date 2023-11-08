fa20eaa73e5714aa1754aa1263efb6c5073abc40
Akanga  Andrew <sseluyindaeva@gmail.com>
2023-11-08 09:45:31 +0300

    fibonnaci numbers using recursive
    i remove the second function to make the code more understandable

--- Fibonacci numbers.c
+++ Fibonacci numbers.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 int fibonacci (int num);
-void next_fibonacci(int limit);//I change it to void since I have nothing to return
 int main(){
-    int a;
+    int a,i;
+
     printf("Enter the limit of finding fibonacci numbers:\n");
     scanf("%d", &a);
-    printf("The sequence fibonacci numbers  to the limit %d is %d", a);
-    next_fibonacci(a);
+    printf("The sequence fibonacci numbers  to the limit %d is\n", a);
+   for(i=0;i<=a;i++){
+       printf("%d\t", fibonacci(i));
+
+   }
+
     return 0;
 }
 int fibonacci (int num){
-    if (num <= 0){
-        return num;//if the number is less or equal to zero  return that number as the fibonaccci
+    if (num <=1){
+        return num;//this is to cover for 1 and 0
     }else{
-        return fibonacci(num-2)+fibonacci(num-1);//if it's greater than zero then the fibonacci number is f(n-2)+f(n-1)
+        return fibonacci(num-1)+fibonacci(num-2);//if it's greater than zero then the fibonacci number is f(n-2)+f(n-1)
     }
 }
-void next_fibonacci(int limit){
-    for (int i = 1;i<limit;i++){
-        printf("%d", fibonacci(i));
 
-    }
-}
+
