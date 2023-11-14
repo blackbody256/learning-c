cd1f718be435a89fc713b21873ecb9a7d97650d3
Akanga  Andrew <sseluyindaeva@gmail.com>
2023-11-14 15:28:24 +0300

    2D array of strings
    I practiced use of arrays of strings including how to switch the members of arrays

---
+++ array of strings.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+int main(){
+    char cars[3][10]={"camaro", "Benz", "mustang"};
+    //cars[benz]="tesla"; 
+    /*To modify the elements of the 2d array we cant just assign like usually
+    thats why i included string.header so that i use a function string copy (strcpy)*/
+    strcpy(cars[0], "tesla");  
+    for (int i =0; i< (sizeof(cars)/sizeof(cars[0])); i++){
+        printf("%s", cars[i]);
+    }
+
+}
